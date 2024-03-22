c9a65ecde40820a59ff43dcba403fd6b261e2f83
Madeleine Di Gregorio <mdigreg3@uwo.ca>
2024-03-22 15:16:39 -0400

    Final Demo: working C-Arm manipulation on console and DRR capture, multi-user functionality

--- C-Arm Simulator/Assets/Scripts/TwistCarm.cs
+++ C-Arm Simulator/Assets/Scripts/TwistCarm.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.XR.ARFoundation;
+// using UnityEngine.XR.ARFoundation;
 
 public class TwistCarm : MonoBehaviour
 {
